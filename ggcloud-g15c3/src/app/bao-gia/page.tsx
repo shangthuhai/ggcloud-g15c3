63aebef0dbdf7da46e378c371bb54773a506caa0
pthao0403 <phuongthao05092021@gmail.com>
2026-06-02 12:06:24 +0700

    Update booking page UI

---
+++ ggcloud-g15c3/src/app/bao-gia/page.tsx
@@ -0,0 +1,13 @@
+export default function RequestQuotePage() {
+  return (
+    <div className="w-full min-h-screen py-10 bg-gray-50 flex flex-col items-center">
+      <h2 className="text-xl font-bold mb-6">Yêu Cầu Quà Tặng Số Lượng Lớn</h2>
+      <div className="w-full max-w-3xl bg-white shadow-md rounded-xl overflow-hidden" style={{ height: '600px' }}>
+        <iframe aria-label='Form thu thập thông tin ' frameBorder="0"
+        style={{ height: '500px', width: '99%', border: 'none' }}
+          src='https://forms.zohopublic.com/phuongthao05092021gm1/form/ClientDetails/formperma/zdbYXr-T81fcwU4ehk3KyUujkX5bxVFoH-DlfPiWkhA' >
+</iframe>
+      </div>
+    </div>
+  );
+}
